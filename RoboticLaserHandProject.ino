a08977861e83163451fc417f1b55487ec611a0d4
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2021-06-29 14:43:34 -0400

    Added code for vertical motion from new servo motor.

--- RoboticLaserHandProject.ino
+++ RoboticLaserHandProject.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 
-// instantiating the servo motor.
-Servo myservo;
+// instantiating the two servo motors.
+Servo myservo, myservo2;
 
 // declaring variables for pin numbers.
 const int SW_pin = 2;
@@ -11,8 +11,11 @@ const int Laser_pin = 12;
 
 void setup() {
   
-  // attaching the servo motor to pin 10.
+  // attaching the first servo motor to pin 10.
   myservo.attach(10);
+
+  // attaching the second servo motor to pin 11.
+  myservo2.attach(11);
   
   // initializing the switch pin as input.
   pinMode(SW_pin, INPUT);
@@ -28,8 +31,8 @@ void setup() {
 
   }
   
-  // declaring the servo's initial position as 90 degrees.
-  int servopos1 = 90;
+  // declaring the servo 1's and servo 2's initial position as 90 degrees.
+  int servopos1, servopos2 = 90;
 
 // MAIN-LOOP.
 void loop() {
@@ -37,12 +40,14 @@ void loop() {
   // reading the x and y coordinates of the joystick.
   int x_cor = analogRead(X_pin);
   int y_cor = analogRead(Y_pin);
+  
+  // creating variables to track the rate of increment for every angle movement.
   int angle1, angle2;
   
-  // creating an if-statement to check for movement from the joystick.
+  // creating an if-statement to check for horizontal movement from the joystick.
   if (x_cor <= 450 or x_cor >= 550){
   
-    // if the joystick is moved, mapping the angle to the x-coordinate.
+    // if the joystick is moved, mapping the angle to the appropriate x-coordinate.
     angle1 = map(x_cor, 0, 1023, -30, 30);
     servopos1 = servopos1 + angle1;
     
@@ -51,19 +56,24 @@ void loop() {
     if (servopos1 < 0) servopos1 = 0;
   }
   
-  // if-statement that does the same job as the previous one. 
-  // **I'LL BE MAKING CHANGES TO THIS IF-STATEMENT IN THE NEXT WORKING ROUND."
+  // creating an if-statement to check for vertical movement from the joystick.
   if (y_cor <= 450 or y_cor >= 550){
 
+    // if the joystick is moved, mapping the angle to the appopriate y-coordinate.
     angle2 = map(y_cor, 0, 1023, -30, 30);
-    servopos1 = servopos1 + angle2;
+    servopos2 = servopos2 + angle2;
 
-    if (servopos1 > 180) servopos1 = 180;
-    if (servopos1 < 0) servopos1 = 0;
+     // stopping the movement of the servo motor with the laser if the position is 180 or 0.
+    if (servopos2 > 180) servopos2 = 180;
+    if (servopos2 < 0) servopos2 = 0;
   }
   
-  // writing the position to move to.
+  // writing the position to move to for each servo.
+  // servo controlling the x-movement.
   myservo.write(servopos1);
+
+  // servo controlling the y-movement.
+  myservo2.write(servopos2);
   
   // creating an if-statement to shine the laser when the joystick is pressed.
   if (digitalRead(SW_pin) == LOW){
@@ -74,6 +84,7 @@ void loop() {
   else {
 
     digitalWrite(Laser_pin, LOW);
+    
     delay(500);
   }
  
